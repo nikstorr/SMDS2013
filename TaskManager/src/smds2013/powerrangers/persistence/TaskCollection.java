d80696077a3c45d0354866cbd1ce069fe9b3a88e
Mikkel <mbyb@itu.dk>
2013-10-24 13:31:03 +0200

    masharl client

--- TaskManager/src/smds2013/powerrangers/persistence/TaskCollection.java
+++ TaskManager/src/smds2013/powerrangers/persistence/TaskCollection.java
@@ -1,17 +1,23 @@
 package smds2013.powerrangers.persistence;
 
 import java.io.Serializable;
+import java.util.ArrayList;
 import java.util.List;
 import javax.xml.bind.annotation.XmlElement;
 import javax.xml.bind.annotation.XmlElementWrapper;
 import javax.xml.bind.annotation.XmlRootElement;
 
-@XmlRootElement(name = "tasks")
+@XmlRootElement(name = "taskmanager")
 public class TaskCollection implements Serializable {
     
     @XmlElementWrapper(name = "tasks")    
     @XmlElement(name = "task")
     public List<Task> tasks;
+    
+    public TaskCollection()
+    {
+    	tasks = new ArrayList<Task>();
+    }
     
     public TaskCollection(List<Task> list) {
         this.tasks = list;

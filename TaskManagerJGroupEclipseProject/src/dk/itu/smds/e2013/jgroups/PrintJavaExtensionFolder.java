4f12e09497cd51a805c989a55e79a3f5b86b145c
nikstorr <nikstorr@gmail.com>
2013-10-30 14:57:29 +0100

    JGRoupsProject

---
+++ TaskManagerJGroupEclipseProject/src/dk/itu/smds/e2013/jgroups/PrintJavaExtensionFolder.java
@@ -0,0 +1,22 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package dk.itu.smds.e2013.jgroups;
+
+/**
+ *
+ * @author Rao
+ */
+public class PrintJavaExtensionFolder {
+    
+    
+        public static void main(String[] args) throws Exception {
+
+
+        // The following statement will print the path to Java extensions folder.
+      
+        System.out.println(System.getProperty("java.ext.dirs"));
+    }
+    
+}
